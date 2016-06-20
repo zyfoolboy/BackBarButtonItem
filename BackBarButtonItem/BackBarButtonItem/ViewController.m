529098b4d8f02daddf3ef34c2d5db41ceae649b1
zyfoolboy <deyi@deyideMac-mini.local>
2016-06-20 11:49:43 +0800

    first commit

--- BackBarButtonItem/BackBarButtonItem/ViewController.m
+++ BackBarButtonItem/BackBarButtonItem/ViewController.m
@@ -17,7 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIImage *backButtonImage = [[UIImage imageNamed:@"iconfont-fanhui"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    [[UIBarButtonItem appearance] setBackButtonBackgroundImage:backButtonImage  forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    [[UIBarButtonItem appearance] setBackButtonTitlePositionAdjustment:UIOffsetMake(0, -backButtonImage.size.height*3) forBarMetrics:UIBarMetricsDefault];
+    self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:0.000 green:0.858 blue:1.000 alpha:1.000];
 }
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
